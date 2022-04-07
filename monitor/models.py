197ecfc7b01fc14b8ec004a3015f1cc317123b06
Shubh Pathak <shubhpathak07@gmail.com>
2022-04-07 10:45:22 +0530

    feat:added device model

--- monitor/models.py
+++ monitor/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class Device(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    unique_id = models.PositiveIntegerField()
+    lat = models.CharField(max_length=20)
+    long = models.CharField(max_length=20)
+    data = models.ManyToManyField('SensorValue')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return (self.unique_id)
+
+class SensorValue(models.Model):
+    temperature = models.CharField(max_length=21)
+    humidity = models.CharField(max_length=21)
+    ir = models.CharField(max_length=21)
+    light = models.CharField(max_length=21)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-timestamp']
+
+    def __str__(self):
+        return (self.timestamp)
